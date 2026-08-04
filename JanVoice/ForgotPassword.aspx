cb28a0a1e43146636fe60a89718e15cd52dd88f7
ritesh33-dotcom <rj4657345@gmail.com>
2026-08-04 20:06:23 +0530

    dashboard page repairing start

---
+++ JanVoice/ForgotPassword.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Site.Master" AutoEventWireup="true" CodeBehind="ForgotPassword.aspx.cs" Inherits="JanVoice.ForgotPassword" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <link href="CSS/forgotpassword.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="forgot-page">
+
+    <div class="forgot-card">
+
+        <h2>Forgot Password</h2>
+
+        <p>
+            Enter your registered email and create a new password.
+        </p>
+
+        <div class="form-group">
+
+            <label>Email</label>
+
+            <asp:TextBox
+                ID="txtEmail"
+                runat="server"
+                CssClass="form-control"
+                TextMode="Email" />
+
+        </div>
+
+        <div class="form-group">
+
+            <label>New Password</label>
+
+            <asp:TextBox
+                ID="txtNewPassword"
+                runat="server"
+                CssClass="form-control"
+                TextMode="Password" />
+
+        </div>
+
+        <div class="form-group">
+
+            <label>Confirm Password</label>
+
+            <asp:TextBox
+                ID="txtConfirmPassword"
+                runat="server"
+                CssClass="form-control"
+                TextMode="Password" />
+
+        </div>
+
+        <asp:Button
+            ID="btnResetPassword"
+            runat="server"
+            Text="Reset Password"
+            CssClass="reset-btn"
+            OnClick="btnResetPassword_Click" />
+
+    </div>
+
+</div>
+
+</asp:Content>
---
+++ JanVoice/ForgotPassword.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JanVoice
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+    {
+        string connectionString =
+                ConfigurationManager.ConnectionStrings["JanVoiceDB"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text.Trim() == "")
+            {
+                ClientScript.RegisterStartupScript(
+                    this.GetType(),
+                    "alert",
+                    "alert('Please enter your registered email.');",
+                    true);
+
+                return;
+            }
+
+            if (txtNewPassword.Text.Trim() == "")
+            {
+                ClientScript.RegisterStartupScript(
+                    this.GetType(),
+                    "alert",
+                    "alert('Please enter new password.');",
+                    true);
+
+                return;
+            }
+
+            if (txtConfirmPassword.Text.Trim() == "")
+            {
+                ClientScript.RegisterStartupScript(
+                    this.GetType(),
+                    "alert",
+                    "alert('Please confirm your password.');",
+                    true);
+
+                return;
+            }
+
+            if (txtNewPassword.Text != txtConfirmPassword.Text)
+            {
+                ClientScript.RegisterStartupScript(
+                    this.GetType(),
+                    "alert",
+                    "alert('Passwords do not match.');",
+                    true);
+
+                return;
+            }
+            using (SqlConnection con =
+    new SqlConnection(connectionString))
+            {
+                string query = @"
+
+        SELECT COUNT(*)
+
+        FROM Users
+
+        WHERE Email=@Email";
+
+                SqlCommand cmd =
+                    new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue(
+                    "@Email",
+                    txtEmail.Text.Trim());
+
+                con.Open();
+
+                int count =
+                    Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (count == 0)
+                {
+                    ClientScript.RegisterStartupScript(
+                        this.GetType(),
+                        "alert",
+                        "alert('No account found with this email.');",
+                        true);
+
+                    return;
+                }
+                query = @"
+
+                        UPDATE Users
+
+                        SET PasswordHash=@Password
+
+                        WHERE Email=@Email";
+
+                cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue(
+                    "@Password",
+                    txtNewPassword.Text.Trim());
+
+                cmd.Parameters.AddWithValue(
+                    "@Email",
+                    txtEmail.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+
+            }
+
+            ClientScript.RegisterStartupScript(
+    this.GetType(),
+    "success",
+    "alert('Password reset successfully.');window.location='Login.aspx';",
+    true);
+
+        }
+    }
+}
