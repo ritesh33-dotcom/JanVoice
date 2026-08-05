900f6ab3c0de4f1570d972b0efc8930d9809eae9
kadampranav957-eng <kadampranav957@gmail.com>
2026-08-05 21:32:11 +0530

    Offiecr Master Page Inserted suuccesfully

--- JanVoice/MasterPages/Officer.Master.cs
+++ JanVoice/MasterPages/Officer.Master.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +9,68 @@ using System.Web.UI.WebControls;
 
 namespace JanVoice.MasterPages
 {
-    public partial class Officer : System.Web.UI.MasterPage
+
+    public partial class Officer : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CheckOfficerLogin();
+                LoadOfficerInfo();
+            }
+        }
 
+        private void CheckOfficerLogin()
+        {
+            if (Session["OfficerID"] == null)
+            {
+                Response.Redirect("~/OfficerLogin.aspx");
+            }
+        }
+
+        private void LoadOfficerInfo()
+        {
+            try
+            {
+                string cs = ConfigurationManager.ConnectionStrings["JanVoiceDB"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    string query = @"SELECT FullName
+                                     FROM Users
+                                     WHERE UserID=@UserID";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@UserID",
+                        Session["OfficerID"]);
+
+                    con.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                        lblOfficerName.Text = dr["FullName"].ToString();
+                    }
+
+                    dr.Close();
+                }
+            }
+            catch
+            {
+                lblOfficerName.Text = "Officer";
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+
+            Session.Abandon();
+
+            Response.Redirect("~/Login.aspx");
         }
     }
 }
